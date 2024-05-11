fca33a42fe0547044d4e101323bb60c7b67d3238
AntoNicksonJ <nicksonjj55@gmail.com>
2024-05-11 23:52:38 +0530

    Changes

--- server/server.py
+++ server/server.py
@@ -1,1 +1,11 @@
- 
+from dotenv import load_dotenv
+from langchain_community.vectorstores.faiss import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+# from htmlTemplates import css, bot_template, user_template
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
+import openai
+from flask import Flask, request, jsonify
+from flask_cors import CORS
