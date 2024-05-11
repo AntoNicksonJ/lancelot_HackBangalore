6a1e01727fc69dfef6f0e9db27ec0232cb2d8621
AntoNicksonJ <nicksonjj55@gmail.com>
2024-05-11 19:22:10 +0530

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,25 @@
 import React from 'react';
+import './App.css';
+import NavigationBar from './components/Nav/NavigationBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+
+
+  return (
+    <div className='App'>
+  <Router>
+  <Routes>
+  <Route path="/" element={
+  <React.Fragment>
+    <NavigationBar/>
+  </React.Fragment>
+
+} />
+  </Routes>
+  </Router>
+    </div>
+  );
+} 
+export default App;
---
+++ client/src/components/Nav/NavigationBar.js
@@ -0,0 +1,75 @@
+import './NavigationBar.css'
+import { useState, useEffect } from 'react'
+import { RxHamburgerMenu as OpenMenu } from "react-icons/rx"
+import { CgClose as HideMenu} from 'react-icons/cg' 
+import { Link } from 'react-router-dom';
+
+
+const NavigationBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth)
+      window.innerWidth > 1200 && setMenuOpen(false)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return (
+    <>
+      <nav className="navBar">
+        <div className="leftSection">
+          <a href='https://github.com/KennethOnuorah/Website' target={'_blank'} style={{textDecoration: "none", outline: "none"}}>
+            <div className='companyLogo'>
+              {/* <img src=''/> */}
+              <h1>Apply</h1>
+            </div>
+          </a>
+            <div className="navBtns">
+          
+            <a href="#"><Link to="/">Home</Link></a>            
+        {/* <li><a href="#"><Link to="/Irc">IRC 2024</Link></a></li>   */}
+
+
+              <div className="navDropdown">
+              <a href="#"><Link to="/home">Scheam</Link></a> 
+                {/* <div className="aboutDropdownMenu">
+                </div> */}
+              </div>
+              <div className="navDropdown">
+              <a href="#"><Link to="/detail">Project</Link></a> 
+                {/* <div className="serviceDropdownMenu">
+                 
+                </div> */}
+              </div>
+              
+              
+              {/* <button>Blog</button>
+              <button>News</button> */}
+            </div>
+        </div>
+        <div className='rightSection'>
+          <div className="accountSection">
+            <button>Login</button>
+            <button>Sign Up</button>
+          </div>
+          <div className="menuSection">
+            {
+              !menuOpen ? 
+                <OpenMenu className='menuBtn' onClick={() => setMenuOpen(true)}/> : 
+                <HideMenu className='hideBtn' onClick={() => setMenuOpen(false)}/>
+            }
+          </div>
+        </div>
+        
+      </nav>
+    </>
+  )
+}
+
+export default NavigationBar
